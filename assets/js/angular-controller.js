338724c0b294b359f5ddbfb8a5aec843b2a94fac
esaavedraa1 <esaavedra@comseg.cl>
2016-12-05 14:11:52 -0400

    crear moneda

--- assets/js/angular-controller.js
+++ assets/js/angular-controller.js
@@ -824,7 +824,7 @@ colorAdminApp.controller('MonedaCrearController',function($scope, $rootScope, $s
         $scope.valido = 1;
 
         if($scope.valido==1){
-            bootbox.alert($scope.sgt_monedas);
+            bootbox.alert("ERROR" + $scope.sgt_monedas);
             $scope.guardarMoneda();
         }
     };
