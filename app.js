51c768bd0339199044eceae38778ebbc1502dcf8
esaavedraa1 <esaavedra@comseg.cl>
2016-12-07 15:09:53 -0400

    app.js proveedores

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ var mongoose = require('mongoose');
 require('mongoose-double')(mongoose);
 var SchemaTypes = mongoose.Schema.Types;
 var Schema = mongoose.Schema;
+
 mongoose.connect('mongodb://localhost/SGTDB', function(error){
     if(error){
         throw error;
@@ -44,7 +45,9 @@ mongoose.connect('mongodb://localhost/SGTDB', function(error){
 });
 
 
-
+/*-------------------------------------------------------
+1.0 Moneda Tipo Controller
+---------------------------------------------------------*/
 var Sgt_moneda_tipoSchema = mongoose.Schema({
     monet_id: Number,
     monet_nombre: String,
@@ -53,21 +56,9 @@ var Sgt_moneda_tipoSchema = mongoose.Schema({
 
 });
 var Sgt_moneda_tipo = mongoose.model('Sgt_moneda_tipo', Sgt_moneda_tipoSchema);
-//-----------------------------------------------
-
-var Sgt_monedaSchema = mongoose.Schema({
-    monet_id: {type: Schema.ObjectId, ref:"Sgt_moneda_tipo"},
-    mone_fecha: { type : Date, default: Date.now},
-    mone_valor: String
-});
-var Sgt_moneda = mongoose.model('Sgt_moneda', Sgt_monedaSchema);
-
-
-
 app.get('/Monedas_Tipo', function(req, res){
     res.sendfile('./public/index2.html');
 });
-
 app.get('/Monedas_Tipo/listar', function(req, res){
     Sgt_moneda_tipo.find({}, function(error, sgt_monedas_tipo){
         if(error){
@@ -77,7 +68,6 @@ app.get('/Monedas_Tipo/listar', function(req, res){
         }
     })
 });
-
 app.get('/Monedas_Tipo/recuperar', function(req, res){
     Sgt_moneda_tipo.findById(req.query._id, function(error, sgt_monedas_tipo){
         if(error){
@@ -112,7 +102,7 @@ app.post('/Monedas_Tipo/guardar', function(req, res){
                 res.send('Error.');
             }else{
                 var sgt_moneda_tipo = documento;
-                sgt_moneda_tipo.monet_id = req.query.monet_id,
+                    sgt_moneda_tipo.monet_id = req.query.monet_id,
                     sgt_moneda_tipo.monet_nombre= req.query.monet_nombre,
                     sgt_moneda_tipo.monet_codigo= req.query.monet_codigo,
                     sgt_moneda_tipo.monet_pais = req.query.monet_pais
@@ -136,7 +126,15 @@ app.post('/Monedas_Tipo/eliminar', function(req, res){
         }
     });
 });
-//--------------------------------------------------------
+/*-------------------------------------------------------
+ 2.0 Moneda  Controller
+ ---------------------------------------------------------*/
+var Sgt_monedaSchema = mongoose.Schema({
+    monet_id: {type: Schema.ObjectId, ref:"Sgt_moneda_tipo"},
+    mone_fecha: { type : Date, default: Date.now},
+    mone_valor: String
+});
+var Sgt_moneda = mongoose.model('Sgt_moneda', Sgt_monedaSchema);
 app.get('/Monedas', function(req, res){
     res.sendfile('./public/index2.html');
 });
@@ -184,7 +182,7 @@ app.post('/Monedas/guardar', function(req, res){
             if(error){
                 res.send('Error.');
             }else{
-                var sgt_moneda_tipo = documento;
+                var sgt_moneda = documento;
                     sgt_moneda.monet_id = req.query.monet_id,
                     sgt_moneda.mone_fecha= req.query.mone_fecha,
                     sgt_moneda.mone_valor= req.query.mone_valor,
@@ -208,6 +206,9 @@ app.post('/Monedas/eliminar', function(req, res){
         }
     });
 });
+/*-------------------------------------------------------
+ 3.0 Proveedor Tipo Controller
+ ---------------------------------------------------------*/
 var Sgt_proveedor_tipoSchema = mongoose.Schema({
     provt_tipo: String
 
@@ -225,7 +226,6 @@ app.get('/Proveedores_Tipo/listar', function(req, res){
         }
     })
 });
-
 app.get('/Proveedores_Tipo/recuperar', function(req, res){
     Sgt_proveedor_tipo.findById(req.query._id, function(error, sgt_proveedores_tipo){
         if(error){
@@ -237,7 +237,6 @@ app.get('/Proveedores_Tipo/recuperar', function(req, res){
         }
     });
 });
-
 app.post('/Proveedores_Tipo/guardar', function(req, res){
     if(req.query._id == null){
         //Inserta
@@ -270,7 +269,6 @@ app.post('/Proveedores_Tipo/guardar', function(req, res){
         });
     }
 });
-
 app.post('/Proveedores_Tipo/eliminar', function(req, res){
     Sgt_proveedor_tipo.remove({_id: req.query._id}, function(error){
         if(error){
@@ -280,8 +278,128 @@ app.post('/Proveedores_Tipo/eliminar', function(req, res){
         }
     });
 });
-
-
+/*-------------------------------------------------------
+ 4.0 Proveedor Controller
+ ---------------------------------------------------------*/
+var Sgt_proveedorSchema = mongoose.Schema({
+    provt_id :{type:Shema.ObjectId,ref:"Sgt_proveedor_tipo"},
+    prov_rut : String,
+    prov_origen : String,
+    prov_razon_social : String,
+    prov_giro : String,
+    prov_direccion : String,
+    prov_numero : Number,
+    prov_oficina : String,
+    prov_ranking : Number,
+    prov_telefono : String,
+    prov_anexo : String,
+    prov_email : String,
+    prov_web : String,
+    prov_credito : String,
+    prov_credito_monto : String,
+    prov_horario_inicio : String,
+    prov_horario_termino : String,
+    prov_formulario_30 : String
+});
+var Sgt_proveedor = mongoose.model('Sgt_proveedor', Sgt_proveedorSchema);
+app.get('/Proveedores', function(req, res){
+    res.sendfile('./public/index4.html');
+});
+app.get('/Proveedores/listar', function(req, res){
+    Sgt_proveedor.find({}, function(error, sgt_proveedores){
+        if(error){
+            res.send('Error.');
+        }else{
+            res.send(sgt_proveedores);
+        }
+    })
+});
+app.get('/Proveedores/recuperar', function(req, res){
+    Sgt_proveedor.findById(req.query._id, function(error, sgt_proveedores){
+        if(error){
+            res.send('Error.');
+            console.log("Error en la recuperacion")
+        }else{
+            res.send(sgt_proveedores);
+            console.log(sgt_proveedores)
+        }
+    });
+});
+app.post('/Proveedores/guardar', function(req, res){
+    if(req.query._id == null){
+        //Inserta
+        var sgt_proveedor = new Sgt_proveedor({
+            provt_id: req.query.provt_id,
+            prov_rut : req.query.prov_rut,
+            prov_origen : req.query.prov_origen ,
+            prov_razon_social : req.query.prov_razon_social,
+            prov_giro : req.query.prov_giro,
+            prov_direccion : req.query.prov_direccion,
+            prov_numero : req.query.prov_numero,
+            prov_oficina : req.query.prov_oficina,
+            prov_ranking : req.queryprov_ranking,
+            prov_telefono : req.query.prov_telefono,
+            prov_anexo : req.query.prov_anexo,
+            prov_email : req.query.prov_email,
+            prov_web : req.query.prov_web,
+            prov_credito : req.query.prov_credito,
+            prov_credito_monto : req.query.prov_credito_monto,
+            prov_horario_inicio : req.query.prov_horario_inicio,
+            prov_horario_termino : req.query.prov_horario_termino,
+            prov_formulario_30 : req.query.prov_formulario_30
+        });
+        sgt_proveedor.save(function(error, documento){
+            if(error){
+                res.send('Error.');
+            }else{
+                res.send(documento);
+            }
+        });
+    }else{
+        //Modifica
+        Sgt_proveedor.findById(req.query._id, function(error, documento){
+            if(error){
+                res.send('Error.');
+            }else{
+                var sgt_proveedor = documento;
+                sgt_proveedor.provt_id = req.query.provt_id,
+                    sgt_proveedor.prov_rut = req.query.prov_rut,
+                    sgt_proveedor.prov_origen = req.query.prov_origen ,
+                    sgt_proveedor.prov_razon_social = req.query.prov_razon_social,
+                    sgt_proveedor.prov_giro = req.query.prov_giro,
+                    sgt_proveedor.prov_direccion = req.query.prov_direccion,
+                    sgt_proveedor.prov_numero = req.query.prov_numero,
+                    sgt_proveedor.prov_oficina = req.query.prov_oficina,
+                    sgt_proveedor.prov_ranking = req.queryprov_ranking,
+                    sgt_proveedor.prov_telefono = req.query.prov_telefono,
+                    sgt_proveedor.prov_anexo = req.query.prov_anexo,
+                    sgt_proveedor.prov_email = req.query.prov_email,
+                    sgt_proveedor.prov_web = req.query.prov_web,
+                    sgt_proveedor.prov_credito = req.query.prov_credito,
+                    sgt_proveedor.prov_credito_monto = req.query.prov_credito_monto,
+                    sgt_proveedor.prov_horario_inicio = req.query.prov_horario_inicio,
+                    sgt_proveedor.prov_horario_termino = req.query.prov_horario_termino,
+                    sgt_proveedor.prov_formulario_30 = req.query.prov_formulario_30
+                sgt_proveedor.save(function(error, documento){
+                    if(error){
+                        res.send('Error.');
+                    }else{
+                        res.send(documento);
+                    }
+                });
+            }
+        });
+    }
+});
+app.post('/Proveedores/eliminar', function(req, res){
+    Sgt_proveedor.remove({_id: req.query._id}, function(error){
+        if(error){
+            res.send('Error.');
+        }else{
+            res.send('Ok');
+        }
+    });
+});
 
 app.listen(8080, function() {
     console.log('App listening on port 8080');
