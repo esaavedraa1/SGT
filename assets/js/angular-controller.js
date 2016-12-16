a00040c48ac8677e56c9d6bc20f1cf77934d8290
esaavedraa1 <esaavedra@comseg.cl>
2016-12-16 17:10:18 -0400

    monet tipo to datatable

--- assets/js/angular-controller.js
+++ assets/js/angular-controller.js
@@ -534,7 +534,7 @@ colorAdminApp.controller('tablaMonedaTipoDefaultController', function($scope, $r
                             responsive: true,
 
                         });
-                        var count = table.rows( { selected: true } ).data();
+                        var count = table.rows( { selected: true } ).id();
                         $scope.editar = function () {
 
                             console.log(count + "celdas seleccionadas");
