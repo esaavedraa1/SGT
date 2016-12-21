6ab666d7274d080bee1485a89ee77eeab32362f1
esaavedraa1 <esaavedra@comseg.cl>
2016-12-21 08:38:00 -0400

    change route of moneda tipo to moneda todos $location

--- assets/js/angular-controller.js
+++ assets/js/angular-controller.js
@@ -1296,7 +1296,7 @@ colorAdminApp.controller('MonedaTipoCrearController',function($scope, $rootScope
             if(typeof(data) == 'object'){
                 $scope.limpiarDatos();
                 bootbox.alert("Envio Exitoso, Informacion Almacenada ");
-                $window.location.href='#/app/moneda_tipo/todos';
+                $window.location.href='#/app/moneda/tipo/todos';
             }else{
                 alert('Error al intentar guardar la Moneda Tipo.');
             }
