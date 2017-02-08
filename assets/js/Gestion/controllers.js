5b0a18ae4b113a7f8c5bd906e61ee078af37705d
esaavedraa1 <esaavedra@comseg.cl>
2017-02-08 16:46:49 -0300

    30200

---
+++ assets/js/Gestion/controllers.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Core i3 on 08-02-2017.
+ */
+colorAdminApp.controller('30201_Controller', function($scope, $rootScope, $state) {
+    $scope.PRO_ACEP_OBJ=function()
+    {
+        bootbox.prompt("Ingrese Nueva Tarifa",function (result) {
+            console.log(result);
+        });
+    }
+    $scope.PRO_RECH_OBJ=function()
+    {
+        bootbox.alert("Viaje vuelve a Estado CERRADO");
+    }
+});
