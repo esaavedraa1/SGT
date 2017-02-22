fa480e55bb940bf72e8a587bd51d4d97b54b98cd
esaavedraa1 <esaavedra@comseg.cl>
2017-02-22 15:03:42 -0300

    30601

--- assets/js/angular-app.js
+++ assets/js/angular-app.js
@@ -1296,6 +1296,46 @@ colorAdminApp.config(['$stateProvider', '$urlRouterProvider', function($statePro
             data: { pageTitle: 'Itininerario de Salida' },
             templateUrl: 'views/Gestion/Validaciones/itinerario.html'
         })
+        //30600: MENU MANIFIESTOS
+        .state('app.gestion.manifiestos', {
+            url: '/manifiestos',
+            template: '<div ui-view></div>',
+            abstract: true
+        })
+        //30601: PANTALLA CERRAR VIAJE
+        .state('app.gestion.manifiestos.cerrar', {
+            url: '/cerrar',
+            data: { pageTitle: 'Cerrar Manifiesto' },
+            templateUrl: 'views/Gestion/Manifiesto/cerrar.html',
+            resolve: {
+                service: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        serie: true,
+                        files: [
+                            'assets/plugins/bootbox/bootbox.min.js',
+                            'assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker.css',
+                            'assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.css',
+                            'assets/plugins/ionRangeSlider/css/ion.rangeSlider.css',
+                            'assets/plugins/ionRangeSlider/css/ion.rangeSlider.skinNice.css',
+                            'assets/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css',
+                            'assets/plugins/bootstrap-daterangepicker/daterangepicker.css',
+                            'assets/plugins/select2/dist/css/select2.min.css',
+                            'assets/plugins/bootstrap-eonasdan-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
+                            'assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
+                            'assets/plugins/ionRangeSlider/js/ion-rangeSlider/ion.rangeSlider.min.js',
+                            'assets/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js',
+                            'assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput.min.js',
+                            'assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput-typeahead.js',
+                            'assets/plugins/jquery-tag-it/js/tag-it.min.js',
+                            'assets/plugins/bootstrap-daterangepicker/moment.js',
+                            'assets/plugins/bootstrap-daterangepicker/daterangepicker.js',
+                            'assets/plugins/select2/dist/js/select2.min.js',
+                            'assets/plugins/bootstrap-eonasdan-datetimepicker/build/js/bootstrap-datetimepicker.min.js'
+                        ]
+                    });
+                }]
+            }
+        })
         //30800: MENU VIAJES
         .state('app.gestion.viaje', {
             url: '/viaje',
