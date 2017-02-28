b5d1660d5c1f8310233baa5792d2cd1b3682604b
esaavedraa1 <esaavedra@comseg.cl>
2017-02-28 16:24:52 -0300

    30101

--- assets/js/Gestion/controllers.js
+++ assets/js/Gestion/controllers.js
@@ -8,7 +8,7 @@ colorAdminApp.controller('30101_Controller', function($scope, $rootScope, $state
 
         $('#datetimepicker1').datepicker({
             minViewMode: 1,
-            format : 'MM/YY',
+            format : 'MOM/YEAR',
             clearBtn: true,
             language: 'es'
         });
