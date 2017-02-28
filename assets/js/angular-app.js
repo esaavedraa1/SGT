e64ae62b1b41d742bd40c32a0d12e3945d026d0e
esaavedraa1 <esaavedra@comseg.cl>
2017-02-28 10:11:43 -0300

    30204

--- assets/js/angular-app.js
+++ assets/js/angular-app.js
@@ -1136,6 +1136,40 @@ colorAdminApp.config(['$stateProvider', '$urlRouterProvider', function($statePro
                 }]
             }
         })
+        //30203: PANTALLA PUBLICAR SOLICITUD F-05
+        .state('app.gestion.solicitud.enpatio', {
+            url: '/enpatio',
+            data: { pageTitle: 'Solicitud Express ' },
+            templateUrl: 'views/Gestion/Solicitud/enpatio.html',
+            resolve: {
+                service: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        serie: true,
+                        files: [
+                            'assets/plugins/bootbox/bootbox.min.js',
+                            'assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker.css',
+                            'assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.css',
+                            'assets/plugins/ionRangeSlider/css/ion.rangeSlider.css',
+                            'assets/plugins/ionRangeSlider/css/ion.rangeSlider.skinNice.css',
+                            'assets/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css',
+                            'assets/plugins/bootstrap-daterangepicker/daterangepicker.css',
+                            'assets/plugins/select2/dist/css/select2.min.css',
+                            'assets/plugins/bootstrap-eonasdan-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
+                            'assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
+                            'assets/plugins/ionRangeSlider/js/ion-rangeSlider/ion.rangeSlider.min.js',
+                            'assets/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js',
+                            'assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput.min.js',
+                            'assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput-typeahead.js',
+                            'assets/plugins/jquery-tag-it/js/tag-it.min.js',
+                            'assets/plugins/bootstrap-daterangepicker/moment.js',
+                            'assets/plugins/bootstrap-daterangepicker/daterangepicker.js',
+                            'assets/plugins/select2/dist/js/select2.min.js',
+                            'assets/plugins/bootstrap-eonasdan-datetimepicker/build/js/bootstrap-datetimepicker.min.js'
+                        ]
+                    });
+                }]
+            }
+        })
         //30300 : Menu CONTROL
         .state('app.gestion.control', {
             url: '/control',
