d15442df3b766f4a3ec5274ca7816ca14e78163d
esaavedraa1 <esaavedra@comseg.cl>
2017-03-15 12:01:24 -0300

    20502

--- assets/js/Facturacion/controllers.js
+++ assets/js/Facturacion/controllers.js
@@ -13,6 +13,31 @@ colorAdminApp.controller('20501_Controller', function($scope, $rootScope, $state
         bootbox.alert("Viaje sin cambios");
     }
 });
+colorAdminApp.controller('20502_Controller', function($scope, $rootScope, $state) {
+    angular.element(document).ready(function () {
+        /* Datepicker
+         ------------------------- */
+
+        $('#datetimepicker1').datepicker({
+            minViewMode: 1,
+            format : 'mm-yy',
+            clearBtn: true,
+            language: 'es'
+        });
+    });
+    $scope.PRO_CARGA_SOLI=function()
+    {
+        bootbox.alert("Creación Exitosa  Solicitud N°12244");
+
+    }
+    $scope.PRO_CANC=function()
+    {
+        bootbox.confirm("Desea Salir y volver al menu principal?",function (result) {
+            if(result){
+                $window.location.href='#/app/dashboard';}
+        });
+    }
+});
 colorAdminApp.controller('B2B_Fact_Obje_Resp_Controller', function($scope, $rootScope, $state) {
 	$scope.PRO_RECHA_OF_OBJ=function()
     {
